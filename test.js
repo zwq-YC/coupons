8df67e048e77c3b74894cacb891880347182f73c
养乐多 <63436613+codeyangleduo@users.noreply.github.com>
2022-05-10 20:18:42 +0800

    Update test.js

--- test.js
+++ test.js
@@ -13,7 +13,11 @@ function quan(){
             // var json = httpRequest.responseText;//获取到json字符串，还需解析
             var data = res.data.msg // 获取“当日已领取优惠券”
             console.log(data)
-            if (data == "当日已领取优惠券") {
+            if (data == "当日已领取优惠券" || data == "来晚了，券已领完") {
+                var url = 'https://sctapi.ftqq.com/SCT146464T9GEUSrAYZmMZPGpTDPBYI4tu.send?title=success'
+                var http = new XMLHttpRequest();//第一步：建立所需的对象
+                http.open('POST', url, true);//第二步：打开连接  将请求参数写在url中  ps:"http://localhost:8080/rest/xxx"
+                http.send();//第三步：发送请求  将请求参数写在URL中
                 clearInterval(i)
             }
         }
