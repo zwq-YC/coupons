9184aae158f1c1ceae0c1262201651d426b759b0
养乐多 <63436613+codeyangleduo@users.noreply.github.com>
2022-05-10 10:31:24 +0800

    Add files via upload

--- test.js
+++ test.js
@@ -1,25 +1,24 @@
-
-<script src="./jquery/jquery-2.1.1.min.js"></script>
-<script src="./jquery/jquery.pagination.js"></script>
-<script src="./jquery/jquery-form.min.js"></script>
 <script>
-xunhuan()
-function xunhuan(){
-    $.ajax({
+    script.type="text/javascript";  
+    script.src="./jquery/jquery-2.1.1.min.js";       
+    quan()
+    function quan(){
+        $.ajax({
         url: "http://jieyou.pro:9023/jp/crowdFoundingUser/grantFiveYuanCoupon?mobile=18807065942&userId=1075120",
         type: 'get',
         dataType: 'json',
         data: {},
-        success: function (res) {
+        success: function(res) {
             var data = res.data.msg
-            if(data == "成功"){
+            alert(data)
+            if (data == "当日已领取优惠券") {
                 clearInterval(i)
+                alert("领取成功")
             }
         },
-        error: function () {}
-    })
-    var i = setInterval(function(){
-        xunhuan()
-    },10000);
-}
+        error: function() {}
+        })
+    }
+    var i = setInterval("quan()", 10000);
+
 </script>
