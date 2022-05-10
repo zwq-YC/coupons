15c5edbba0f118098e2efc4adba80297acc2957e
养乐多 <63436613+codeyangleduo@users.noreply.github.com>
2022-05-10 16:45:35 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,24 @@
-
-    script.type="text/javascript";  
-    script.src="./jquery/jquery-2.1.1.min.js";       
-    quan()
-    function quan(){
-        $.ajax({
-        url: "http://jieyou.pro:9023/jp/crowdFoundingUser/grantFiveYuanCoupon?mobile=18807065942&userId=1075120",
-        type: 'get',
-        dataType: 'json',
-        data: {},
-        success: function(res) {
-            var data = res.data.msg
+quan()
+function quan(){
+    var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+    var httpRequest = new XMLHttpRequest();//第一步：建立所需的对象
+    httpRequest.open('GET', 'http://jieyou.pro:9023/jp/crowdFoundingUser/grantFiveYuanCoupon?mobile=18807065942&userId=1075120', true);//第二步：打开连接  将请求参数写在url中  ps:"http://localhost:8080/rest/xxx"
+    httpRequest.send();//第三步：发送请求  将请求参数写在URL中
+    /**
+     * 获取数据后的处理程序
+     */
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+            var res = JSON.parse(httpRequest.responseText);
+            // var json = httpRequest.responseText;//获取到json字符串，还需解析
+            var data = res.data.msg // 获取“当日已领取优惠券”
             console.log(data)
             if (data == "当日已领取优惠券") {
                 clearInterval(i)
-                
             }
-        },
-        error: function() {}
-        })
-    }
-    var i = setInterval("quan()", 10000);
+        }
+    };
+}
+var i = setInterval(function(){
+    quan()
+}, 10000);
